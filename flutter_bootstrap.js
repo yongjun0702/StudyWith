6bc887290fa181e53604943d7fa484d5ac00d2aa
yongjun0702 <yongjun0702@users.noreply.github.com>
2024-12-20 14:34:21 +0000

    deploy: fa45df3daf208f7c1dfbc34d0c2e03749e561788

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "784887788"
+    serviceWorkerVersion: "909276255"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -70,9 +70,9 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2e
 "apple-icon.png": "5726f42e45e38b5a4d9050af060888b5",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "15a43c7323b0a044ca57a3534a638073",
+"flutter_bootstrap.js": "0b9bee5800b6e0df7a4f462c38e33035",
 "site.webmanifest": "451b09a8ca5e2463d1b5165966b1c4b0",
-"main.dart.js": "0825c8146cb09d4e791184ae01efac79",
+"main.dart.js": "a504b248674f793ef826100f36c58927",
 "apple-touch-icon.png": "dbcf5d3561970cb953efb1cf26a53bb2",
 "favicon-32x32.png": "839727219e8ea4694bc8453030536058"};
 // The application shell files that are downloaded before a service worker can
