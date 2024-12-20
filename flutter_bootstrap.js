4a7e764ef14c726f1a2219bc381f1e2761a86b90
yongjun0702 <yongjun0702@users.noreply.github.com>
2024-12-20 15:53:01 +0000

    deploy: 0349f256bbf7879933e8b2c3e43302d348494348

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3857665409"
+    serviceWorkerVersion: "1291386825"
   }
 });
