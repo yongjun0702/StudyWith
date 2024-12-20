09ba5793cce750abcd275620f47045cc7eaeaf6c
yongjun0702 <yongjun0702@users.noreply.github.com>
2024-12-20 14:21:53 +0000

    deploy: 4008d2ae4632aa49f71eef72cecd0c6f78c3429d

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -70,8 +70,8 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2e
 "apple-icon.png": "5726f42e45e38b5a4d9050af060888b5",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "e1655ebf14a98f0663130d01b5a81db8",
-"site.webmanifest": "053100cb84a50d2ae7f5492f7dd7f25e",
+"flutter_bootstrap.js": "15a43c7323b0a044ca57a3534a638073",
+"site.webmanifest": "451b09a8ca5e2463d1b5165966b1c4b0",
 "main.dart.js": "0825c8146cb09d4e791184ae01efac79",
 "apple-touch-icon.png": "dbcf5d3561970cb953efb1cf26a53bb2",
 "favicon-32x32.png": "839727219e8ea4694bc8453030536058"};
