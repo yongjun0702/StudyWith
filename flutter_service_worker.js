57dc1fee82070dde7fbe8e61f9e885bea7a498d7
yongjun0702 <yongjun0702@users.noreply.github.com>
2024-12-21 15:25:30 +0000

    deploy: 87f32501747680ac470f3c6a71999753f318220f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -71,10 +71,10 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2e
 "apple-icon.png": "5726f42e45e38b5a4d9050af060888b5",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "1bd74d5059f7048f106c2bb63a116436",
+"flutter_bootstrap.js": "5845f29d387b1d7643e09d88372a6e5f",
 "site.webmanifest": "451b09a8ca5e2463d1b5165966b1c4b0",
 "CNAME": "d61aa39a6449b590b82912182f32a8f0",
-"main.dart.js": "b52fbe464dfaeb896462a90c0641edea",
+"main.dart.js": "2395e91a5ab77ad58015b3f2f6673177",
 "apple-touch-icon.png": "dbcf5d3561970cb953efb1cf26a53bb2",
 "favicon-32x32.png": "839727219e8ea4694bc8453030536058"};
 // The application shell files that are downloaded before a service worker can
